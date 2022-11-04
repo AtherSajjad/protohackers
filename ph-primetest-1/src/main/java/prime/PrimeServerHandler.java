7282165a3823e7dbee12caa33ec550cc0a5f2ac6
Ather Sajjad <athersajjad@Athers-MacBook-Air.local>
2022-11-04 21:58:38 +0530

    refactor prime

--- ph-primetest-1/src/main/java/prime/PrimeServerHandler.java
+++ ph-primetest-1/src/main/java/prime/PrimeServerHandler.java
@@ -8,37 +8,24 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import util.Utils;
 import util.PrimeChecker;
+import util.Utils;
 
 public class PrimeServerHandler extends SimpleChannelInboundHandler<String> {
 	private static final Logger logger = LoggerFactory.getLogger(PrimeServerHandler.class);
 
 	@Override
 	protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
-		if (!Utils.isJSON(msg)) {
+
+		logger.info("Received message " + msg);
+		if (!Utils.isValidRequest(msg)) {
 			logger.error("Received malformed input " + msg);
-			ChannelFuture future = ctx.writeAndFlush("Malformed Response, Not a valid json\n");
+			ChannelFuture future = ctx.writeAndFlush("Malformed Response\n");
 			future.addListener(ChannelFutureListener.CLOSE);
 			return;
 		}
 
 		JSONObject request = new JSONObject(msg);
-		if (!request.has("method") || !request.getString("method").equals("isPrime")) {
-			logger.error("Received malformed input " + msg);
-
-			ChannelFuture future = ctx.writeAndFlush("Malformed Response, Invalid method name\n");
-			future.addListener(ChannelFutureListener.CLOSE);
-			return;
-		}
-
-		if (!request.has("number") || !Utils.isANumber(request)) {
-			logger.error("Received malformed input " + msg);
-
-			ChannelFuture future = ctx.writeAndFlush("Malformed Response, Not a number\n");
-			future.addListener(ChannelFutureListener.CLOSE);
-			return;
-		}
 
 		Number number = request.getNumber("number");
 		JSONObject response = new JSONObject();
--- ph-primetest-1/src/main/java/util/Utils.java
+++ ph-primetest-1/src/main/java/util/Utils.java
@@ -3,6 +3,19 @@ package util;
 import org.json.JSONObject;
 
 public class Utils {
+	public static boolean isValidRequest(String message) {
+		boolean isValidJSON = isJSON(message);
+
+		if (!isValidJSON) {
+			return false;
+		}
+		JSONObject request = new JSONObject(message);
+		boolean isValidMethod = isValidMethod(request);
+		boolean isValidNumber = isValidNumber(request);
+
+		return isValidJSON && isValidMethod && isValidNumber;
+
+	}
 
 	public static boolean isJSON(String message) {
 		try {
@@ -13,15 +26,41 @@ public class Utils {
 		}
 	}
 
-	public static boolean isANumber(JSONObject request) {
+	public static boolean isValidMethod(JSONObject request) {
 		try {
-			if (request.get("number") instanceof Number) {
-				return true;
+			if (!request.has("method")) {
+				return false;
 			}
+
+			if (!(request.get("method") instanceof String)) {
+				return false;
+			}
+
+			if (!request.getString("method").equals("isPrime")) {
+				return false;
+			}
+
+			return true;
 		} catch (Exception e) {
+			return false;
 		}
 
-		return false;
+	}
+
+	public static boolean isValidNumber(JSONObject request) {
+		try {
+			if (!request.has("number")) {
+				return false;
+			}
+
+			if (!(request.get("number") instanceof Number)) {
+				return false;
+			}
+
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
 	}
 
 }
